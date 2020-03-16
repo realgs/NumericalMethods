1098bb042bd1136a89e964c95095b933a7cd2eb5
Dariusz Palt <246808@student.pwr.edu.pl>
2020-03-16 01:50:54 +0100

    ADDED BUBBLE SORT
    ADDED NOT OPTIMIZED BUBBLE SORT ALGHORITM

---
+++ sortingfunctions.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def bubble_sort(array):
+    length = len(array)
+    for i in range(length):
+        for j in range(0, length - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
+
+
+
