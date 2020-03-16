ab5a603428df8bd3ea6ea7c595a3fe463ecc8768
PTyszkowski <piotr.tyszkowski99@gmail.com>
2020-03-16 15:57:45 +0100

    added working function

--- BubbleSort.py
+++ BubbleSort.py
@@ -4,4 +4,20 @@ Created on Mon Mar 16 12:47:40 2020
 
 @author: piotr
 """
+def swap(a,i,j):
+    zp = a[i]
+    a[i] = a[j]
+    a[j] = zp    
 
+a= [1,9,6,3,4,5]
+ 
+def sort (a):
+    n = len(a)
+  
+    for j in range (n):
+        for i in range (n-1):
+            if a[i] > a[i+1]:
+                swap(a,i,i+1)
+sort(a)
+
+print(a)
