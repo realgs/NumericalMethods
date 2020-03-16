bde834ff2c7d9640776526124d2219a0847ee7c8
pwr246812 <marcin.kordas99@gmail.com>
2020-03-16 14:18:08 +0100

    Added second sorting method

--- Sortowanie.py
+++ Sortowanie.py
@@ -6,3 +6,22 @@ def BubbleSort(dane):
                 dane[i], dane[i+1] = dane[i+1], dane[i]
         n -= 1
     return dane
+
+def QuickSort(tablica):
+
+    mniejsze = []
+    rowne = []
+    wieksze = []
+
+    if len(tablica) > 1:
+        piwot = tablica[0]
+        for element in tablica:
+            if element < piwot:
+                mniejsze.append(element)
+            elif element == piwot:
+                rowne.append(element)
+            else:
+                wieksze.append(element)
+        return QuickSort(mniejsze) + rowne + QuickSort(wieksze)
+    else:
+        return tablica
