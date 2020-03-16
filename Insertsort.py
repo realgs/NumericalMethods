c01259d8232377f60dd9157118775ca9a3032c8d
gwiazda774 <gwiazda774@wp.pl>
2020-03-16 23:02:30 +0100

    add insert_sort function to Insertsort file

--- Insertsort.py
+++ Insertsort.py
@@ -6,3 +6,13 @@ def create_tab(size,start,stop):
         nb=rd.randint(start,stop)
         tab.append(nb)
     return tab
+
+def insert_sort(tab):
+    for i in range(1,len(tab)):
+        nb = tab[i]
+        j = i - 1
+        while j >= 0 and tab[j] > nb:
+            tab[j+1] = tab[j]
+            j = j - 1
+        tab[j+1] = nb
+    return tab
