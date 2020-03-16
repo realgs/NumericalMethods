c5d6d2101bf9f34a63dc7030a5b94c84c08282fe
Dariusz Palt <246808@student.pwr.edu.pl>
2020-03-16 15:04:29 +0100

    ADDED QUICK SORT

--- sortingfunctions.py
+++ sortingfunctions.py
@@ -9,3 +9,22 @@ def bubble_sort(array):
             if copy[j] > copy[j + 1]:
                 copy[j], copy[j + 1] = copy[j + 1], copy[j]
     return copy
+
+
+def partition(array, left, right):
+    i = left - 1
+    pivot = array[right]
+    for j in range(left, right):
+        if array[j] < pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[right], array[i + 1] = array[i + 1], array[right]
+    return i + 1
+
+
+def quick_sort(array, left, right):
+    if left < right:
+        index = partition(array, left, right)
+        quick_sort(array, left, index - 1)
+        quick_sort(array, index + 1, right)
+  
