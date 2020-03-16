1823721849e84f023bb89cad20d8736003f72512
gwiazda774 <gwiazda774@wp.pl>
2020-03-16 22:57:40 +0100

    add bubble_sort function to Bubblesort file

--- Bubblesort.py
+++ Bubblesort.py
@@ -7,3 +7,14 @@ def create_tab(size,start,stop):
         tab.append(nb)
     return tab
 
+def bubble_sort(tab):
+    for i in range(len(tab)):
+        j=len(tab)-1
+        while j>i:
+            if tab[j]<tab[j-1]:
+                mem=tab[j]
+                tab[j]=tab[j-1]
+                tab[j-1]=mem
+            j-=1
+    return tab
+
