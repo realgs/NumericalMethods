450acd24cdface9d141ef500d091f3f95baf9f61
pwr246812 <marcin.kordas99@gmail.com>
2020-04-18 23:22:40 +0200

    Added function chart which creates a plot of historical data

--- API.py
+++ API.py
@@ -1,9 +1,15 @@
 import requests
-import time
+import matplotlib.pyplot as plt
+import datetime
+import numpy as np
 
 
-def get_offers(url, currencies, to_console):
-    url += currencies[0]+currencies[1]+'/orderbook.json'
+def get_offers(url, currencies, to_console, category, Id):
+
+    if category == 'trades':
+        url += currencies[0]+currencies[1]+'/'+category+'.json?since='+str(Id)
+    elif category == 'orderbook':
+        url += currencies[0]+currencies[1]+'/'+category+'.json'
     response = requests.get(url).json()
 
     if to_console == True:
@@ -12,24 +18,99 @@ def get_offers(url, currencies, to_console):
 
     return response
 
-#Available currencies: ['PLN', 'EUR', 'USD', 'USDC', 'GBP']
+def chart():
 
-
-def main():
-    currencies = ['BTC', 'PLN']
+    currencies = ['BTC', 'USD']
     url = "https://bitbay.net/API/Public/"
-    get_offers(url, currencies, True)
+    nr = int(input('Podaj numer transakcji, od której chcesz rozpocząć wykres (mniejszy numer = więcej danych na wykresie): \nZaleca się min. 292759 (01.01.2020r.), gdyż mniejszy numer to dłuższy czas oczekiwania. Max numer: 303000\n'))
+    response = get_offers(url, currencies, False, category='trades', Id=nr)
 
-    i = int(input('\nIle razy odświeżyć? (-1 = inf):  '))
+    buys = []
+    sells = []
+    date_buys = []
+    date_sells = []
 
-    while i > 0 or i == -1:
-        if i > 0:
-            i -= 1
-        response = get_offers(url, currencies, False)
-        buying = response['bids'][0][0]
-        selling = response['asks'][0][0]
-        difference = selling*100/buying - 100
-        print('\nW ostatniej parze, oferta sprzedaży jest o', str(difference)+'% droższa od kupna')
-        time.sleep(5)
+    last_id = response[-1]['tid']
 
-main()
+    print('Pierwsza transakcja:', datetime.datetime.fromtimestamp(int(response[-1]['date'])).strftime('%Y-%m-%d %H:%M:%S'))
+    print('\nPobieranie danych...\n')
+
+    while len(response) == 50:
+        response = get_offers(url, currencies, False, category='trades', Id=last_id)
+
+        for i in range(len(response)):
+            if response[i]['type'] == 'buy':
+                buy = response[i]['price']
+                buys.append(buy)
+                date_buy = response[i]['date']
+                date_buys.append(date_buy)
+            else:
+                sell = response[i]['price']
+                sells.append(sell)
+                date_sell = response[i]['date']
+                date_sells.append(date_sell)
+        last_id = response[-1]['tid']
+
+    print('Ostatnia transakcja:', datetime.datetime.fromtimestamp(date_buys[-1]).strftime('%Y-%m-%d %H:%M:%S'))
+
+    dif_b =[0]
+    dift_b = [0]
+
+    for i in range(len(date_buys) - 1):
+        dif_b.append(buys[i + 1] - buys[i])
+        if (date_buys[i + 1] - date_buys[i]) != 0:
+            dift_b.append((buys[i + 1] - buys[i]) / (date_buys[i + 1] - date_buys[i]))
+        else:
+            dift_b.append(buys[i + 1] - buys[i])
+
+    extr_buys = []
+    extr_date_buys = []
+
+    for i in range(len(date_buys) - 1):
+        if dift_b[i] * dift_b[i+1] <= 0:
+            extr_buys.append(buys[i])
+            extr_date_buys.append(date_buys[i])
+
+    dif_s = [0]
+    dift_s = [0]
+
+    for i in range(len(date_sells) - 1):
+        dif_s.append(sells[i + 1] - sells[i])
+        if (date_sells[i + 1] - date_sells[i]) != 0:
+            dift_s.append((sells[i + 1] - sells[i]) / (date_sells[i + 1] - date_sells[i]))
+        else:
+            dift_s.append(sells[i + 1] - sells[i])
+
+    extr_sells = []
+    extr_date_sells = []
+
+    for i in range(len(date_sells) - 1):
+        if dift_s[i] * dift_s[i+1] <= 0:
+            extr_sells.append(sells[i])
+            extr_date_sells.append(date_sells[i])
+
+    plt.subplot(1, 2, 1)
+    plt.plot(date_buys, buys, color='blue', label='Price')
+    plt.plot(date_buys, dif_b, color='red', label='Difference')
+    plt.plot(date_buys, np.gradient(buys), color='black', label='Rate of change')
+    plt.scatter(extr_date_buys, extr_buys, marker='x', color='red', label='Local extremum')
+    plt.xlabel('Date [unix]')
+    plt.ylabel('Price [USD]')
+    plt.title('Buy USD-BTC')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(date_sells, sells, color='blue', label='Price')
+    plt.plot(date_sells, dif_s, color='red', label='Difference')
+    plt.plot(date_sells, np.gradient(sells), color='black', label='Rate of change')
+    plt.scatter(extr_date_sells, extr_sells, marker='x', color='red', label='Local extremum')
+    plt.xlabel('Date [unix]')
+    plt.ylabel('Price [USD]')
+    plt.title('Sell USD-BTC')
+    plt.legend()
+
+    fig = plt.gcf()
+    fig.set_size_inches(36, 14)
+    plt.show()
+
+chart()
