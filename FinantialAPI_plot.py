5fce4f62054b902b0fd3494466ff0f2c05b40e70
Dariusz Palt <246808@student.pwr.edu.pl>
2020-04-18 23:29:49 +0200

    ADDED IMPROVEMENTS TO DOWNLOAD DATA FUNCTION (RECONNECTING WHEN KICKED OUT), ADDED DERIVATIVES DEFFERENCE AND LOCAL EXTREME VALUES FINDER FUNCTION
    You might consider downloading responses.json due to limits on BITBAY API

--- FinantialAPI_plot.py
+++ FinantialAPI_plot.py
@@ -2,9 +2,9 @@ from FinancialAPI import connect_API
 from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
-import json
 from os import path
 import pandas as pd
+import sys
 
 
 def download_data(upd_idx=None, responses=None):
@@ -14,62 +14,103 @@ def download_data(upd_idx=None, responses=None):
         print("UPDATING DATABASE...")
         responses_n = connect_API(url, params, None, upd_idx)
         while len(responses_n) % 50 == 0 and len(responses_n) > 0:
-            responses_n.extend(connect_API(url, params, None, int(responses_n[-1].get("tid"))))
+            try:
+                responses_n.extend(connect_API(url, params, None, since=int(responses_n[-1].get("tid"))))
+            except:
+                print('RECONECTING...')
+                responses_n = pd.DataFrame(responses_n)
+                responses = pd.concat([responses, responses_n], ignore_index=True)
+                save_data(responses)
+                responses = load_data()
+                update(responses)
         print("UPDATE COMPLETED (updated {} rows)".format(int(responses_n[-1].get("tid")) - upd_idx)) if len(
             responses_n) > 0 else print("Nothing to update!")
-        responses_n = pd.DataFrame(responses_n)
-        responses.append(responses_n, ignore_index=True)
-        return responses
-    # TODO saving update changes
-
+        if len(responses_n) > 0:
+            responses_n = pd.DataFrame(responses_n)
+            responses = pd.concat([responses, responses_n], ignore_index=True)
+        else:
+            return responses
     else:
-        responses = connect_API(url, params)
+        total = connect_API(url, params, None, None, 'desc')
+        total = int(total[0]['tid']) + 1
+        responses = connect_API(url, params, )
         print("Downloading data...\nIt will take about 10-15 minutes!!")
         while len(responses) % 50 == 0:
-            responses.extend(connect_API(url, params, None, int(responses[-1].get("tid"))))
-        print("Download completed!")
-    save_data(responses, upd_idx)
+            try:
+                # print(responses[-1].get("tid"))
+                responses.extend(connect_API(url, params, None, int(responses[-1].get("tid"))))
+                done = int(50 * (int(responses[-1].get("tid")) + 1) / total)
+                sys.stdout.write('\r[{}{}]\t{}%'.format('█' * done, '.' * (50 - done), done * 2))
+                sys.stdout.flush()
+            except:
+                print('KICKED OUT! RECONECTING...')
+                responses = pd.DataFrame(responses)
+                save_data(responses)
+                responses = load_data()
+                update(responses)
+        sys.stdout.write('\n')
+        print("\nDownload completed!")
+        responses = pd.DataFrame(responses)
+    save_data(responses)
     return responses
 
 
 def update(responses):
     last_tid = responses.tail(1)["tid"].values[0]
-    print(last_tid)
-    responses = download_data(last_tid, responses)
+    download_data(last_tid, responses)
+    responses = load_data()
     return responses
 
 
-def save_data(data, upd_idx):
-    print(data)
-    if upd_idx:
-        data.to_json(r'responses.json')
-    else:
-        with open('responses.json', 'w') as json_file:
-            json.dump(data, json_file)
+def save_data(data):
+    data.to_json(r'responses.json', orient='records')
 
 
 def load_data():
-    responses = pd.read_json(r'responses.json')
+    responses = pd.read_json(r'responses.json', orient='records')
     return responses
 
 
-def time_change(responses):
-    for response in responses:
-        response['date'] = datetime.utcfromtimestamp(response['date']).strftime('%Y-%m-%d %H:%M:%S')
-    pass
+def difference(data):
+    diff = np.zeros_like(data)
+    for i in range(1, len(data)):
+        diff[i] = data[i] - data[i - 1]
+    return diff
+
+
+def derivative(X, Y):
+    div = np.zeros_like(Y)
+    for i in range(len(Y) - 1):
+        try:
+            div[i] = (Y[i + 1] - Y[i]) / (int(X[i + 1]) / int(X[i]))
+        except ZeroDivisionError:
+            div[i] = 0
+    return div
+
+
+def LocMinMax(derivative, X, Y):
+    Yminmax = []
+    Xminmax = []
+    for i in range(len(derivative) - 1):
+        if derivative[i] * derivative[i + 1] <= 0:
+            Xminmax.append(X[i])
+            Yminmax.append(Y[i])
+    return Xminmax, Yminmax
 
 
 def main():
+    pd.plotting.register_matplotlib_converters()
     if not path.exists('responses.json'):
-        responses = download_data()
+        download_data()
+        responses = load_data()
     else:
         responses = load_data()
         responses = update(responses)
     # print(responses)
-    # start = datetime.strptime(input("Podaj datę rozpoczęcia obserwacji (YYYY-MM-DD): "), '%Y-%m-%d')
-    # end = datetime.strptime(input("Podaj datę zakończenia obserwacji (YYYY-MM-DD): "), '%Y-%m-%d')
-    start = datetime.strptime('2020-01-01', '%Y-%m-%d')
-    end = datetime.strptime('2020-04-18', '%Y-%m-%d')
+    start = datetime.strptime(input("Podaj datę rozpoczęcia obserwacji (YYYY-MM-DD) (od 2014r): "), '%Y-%m-%d')
+    end = datetime.strptime(input("Podaj datę zakończenia obserwacji (YYYY-MM-DD) (do dzisiaj): "), '%Y-%m-%d')
+    # start = datetime.strptime('2020-04-01', '%Y-%m-%d')
+    # end = datetime.strptime('2020-04-19', '%Y-%m-%d')
 
     SellTrades = responses[responses['type'] == 'sell']
     SellTrades = SellTrades[SellTrades['date'] >= start]
@@ -81,18 +122,30 @@ def main():
     attribute_y = 'price'
     XBuy = BuyTrades[attribute_x].values
     YBuy = BuyTrades[attribute_y].values
+    BuyDiff = difference(YBuy)
+    BuyDiv = derivative(XBuy, YBuy)
+    XBMinMax, YBMinMax = LocMinMax(BuyDiv, XBuy, YBuy)
     XSell = SellTrades[attribute_x].values
     YSell = SellTrades[attribute_y].values
-    fig, ax = plt.subplots(1, 2, figsize=(20, 10),dpi=300)
-    ax[0].plot(XBuy, YBuy)
-    ax[0].set_title("Buy")
+    SellDiff = difference(YSell)
+    SellDiv = derivative(XSell, YSell)
+    XSMinMax, YSMinMax = LocMinMax(SellDiv, XSell, YSell)
+    fig, ax = plt.subplots(1, 2, figsize=(30, 10))
+    ax[0].plot(XBuy, YBuy, label='Price')
+    ax[0].plot(XBuy, BuyDiff, label='Difference')
+    ax[0].plot(XBuy, BuyDiv, label='Derivative')
+    ax[0].plot(XBMinMax, YBMinMax, 'x', label='Maxima and minima')
+    ax[0].set_title("Buy offerts BTC-USD")
     ax[0].set_xlabel("Date")
-    ax[0].set_ylabel("Price")
-    ax[1].plot(XSell, YSell)
-    ax[1].set_title("Sell")
+    ax[0].set_ylabel("Price [USD]")
+    ax[1].plot(XSell, YSell, label='Price')
+    ax[1].plot(XSell, SellDiff, label='Difference')
+    ax[1].plot(XSell, SellDiv, label='Derivative')
+    ax[1].plot(XSMinMax, YSMinMax, 'x', label='Maxima and minima')
+    ax[1].set_title("Sell offerts BTC-USD")
     ax[1].set_xlabel("Date")
-    ax[1].set_ylabel("Price")
-    # plt.ylim((0, 20000))
+    ax[1].set_ylabel("Price [USD]")
+    plt.legend()
     plt.show()
 
 
