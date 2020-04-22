65a25aa85017c627def01984a2c8511689a22bc1
ZjemCiBabcie <wojtus.berezowski@gmail.com>
2020-04-22 15:35:21 +0200

    added small changes to the code

--- L4.py
+++ L4.py
@@ -1,25 +1,26 @@
 import numpy as np
 import requests
 import matplotlib.pyplot as plt
+import datetime
 
 
 def plots():
     response = requests.get("https://bitbay.net/API/Public/BTC/trades.json?sort=asc")
     data = response.json()
 
-    print(data)
-
     list_of_prices = []
+    list_of_dates = []
     list_of_extremes = []
     list_of_indexes = []
-
-    time = np.array(np.arange(0, 50))
 
     for i in range(len(data)):
         price = data[i]['price']
         list_of_prices.append(price)
+        date = datetime.datetime.fromtimestamp(data[i]['date']).strftime('%H:%M:%S')
+        list_of_dates.append(date)
 
     list_of_prices = np.array(list_of_prices)
+    list_of_dates = np.array(list_of_dates)
 
     for j in range(len(list_of_prices)):
         if (list_of_prices[j] - list_of_prices[j - 1]) != 0 or (list_of_prices[j] - list_of_prices[j + 1]) != 0:
@@ -29,15 +30,20 @@ def plots():
     list_of_extremes = np.array(list_of_extremes)
     list_of_indexes = np.array(list_of_indexes)
 
-    plt.plot(time, list_of_prices, label='Price plot')
+    plt.plot(list_of_dates, list_of_prices, label='Price plot')
     plt.plot(list_of_indexes, list_of_extremes, 'ro', markersize=2)
 
     plt.xlabel('Time')
     plt.ylabel('Prices')
 
+    plt.xticks(rotation=90, fontsize=9)
+
     plt.title('Price plot')
 
     plt.legend()
+
+    wm = plt.get_current_fig_manager()
+    wm.window.state('zoomed')
 
     plt.show()
 
