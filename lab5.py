340eeed668d625d3e7ee21f5b41eb617f1c4b966
LOXLD <LOXLD@users.noreply.github.com>
2020-05-18 14:21:34 +0200

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -1,41 +1,45 @@
 import requests
 from matplotlib.pyplot import *
 from numpy import *
+import time
+
+var, all_wolumens, rate = [], [], []
 
 
-
-def request():
-    url = "https://api.bitbay.net/rest/trading/transactions/BTC-PLN?limit=300"
+def request(market):
+    url = f"https://api.bitbay.net/rest/trading/transactions/{market}?limit=300"
     response = requests.request("GET", url)
-    return response.json()['items']
+    before60 = float((time.time() + 3600))
+    global rate
+    rate = []
+    for index in response.json()['items']:
+        if float(index['t']) >= before60:
+            rate.insert(0, float(index['r']))
+    global var
+    var = []
+    for x in range(len(rate) - 1):
+        var.append(rate[x] - rate[x - 1])
+    global all_wolumens
+    all_wolumens = []
+    for i in response.json()['items']:
+        if float(i['t']) >= before60:
+            all_wolumens.insert(0, float(i['a']))
+    plotsy(market)
 
-for i in request():
-    print(i["t"])
 
-now = float((time.time() - 3600) * 1000)
+def plotsy(market):
+    subplot(2, 1, 1)
+    title(f'wolumen in last hour in {market}')
+    plot(all_wolumens, c='green')
+    ylabel('wolumen')
+    xlabel('quantity')
 
-rate = []
-for index in request():
-    if float(index['t']) >= now:
-        rate.insert(0, float(index['r']))
+    subplot(2, 1, 2)
+    title('difference of values')
+    plot(var, c='red')
+    ylabel('rate of diffrence')
+    savefig('chart')
+    show()
 
-var = [0]
-for x in range(len(rate) - 1):
-    var.append(rate[x] - rate[x - 1])
 
-all_wolumens = []
-for i in request():
-    if float(i['t']) >= now:
-        all_wolumens.insert(0, float(i['a']))
-
-subplot(2, 1, 1)
-title('wolumens in last hour')
-plot(all_wolumens, c='green')
-ylabel('BTC')
-
-subplot(2, 1, 2)
-title('rate of changes')
-plot(var, c='red')
-ylabel('difference')
-
-show()
+request('LSK-USD')
