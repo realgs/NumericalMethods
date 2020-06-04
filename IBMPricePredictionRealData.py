070b3625fd3810aeb8f12a6a581dca302d495903
PTyszkowski <piotr.tyszkowski99@gmail.com>
2020-06-04 16:39:35 +0200

    improved prediction to real data program, changed answers in txt

--- IBMPricePredictionRealData.py
+++ IBMPricePredictionRealData.py
@@ -1,102 +1,102 @@
 import requests as rq
 import json
 import matplotlib.pyplot as plt
-import scipy.optimize as opt
 import numpy as np
-import datetime
-import random
 
-def API_call(obj):
-
+#calling api for data
+def API_call(obj): 
     obj = rq.get(obj).json()
     text = json.dumps(obj,sort_keys=(True), indent = 4) 
     x = json.loads(text)
     return x
 
-def regresion(x,y):
-    def cost(ab):
-        a,b = ab
-        err = 0
-        for i in range(len(x)):
-            pre_y = a*x[i] + b
-            err += (y[i] - pre_y)**2
-        return err
+#least squares method
+def regresion(xdata, ydata): 
+    if len(xdata.shape) == 1:
+        xN = 1
+    else:
+        xN = xdata.shape[0] 
+    yN = 1
+    X = xdata.reshape(xN,-1)
+    Y = ydata.reshape(yN,-1)
+    X = np.vstack([np.ones_like(X[0]), X])
+    a = np.linalg.inv(X@X.T) @X@Y.T
+    a = np.vstack([a])
+    Y_pr = a.T @ X
+    return (a, Y_pr) 
 
-    r_pocz=np.random.normal(0,10,size=2)
-    r_opt=opt.fmin(cost,r_pocz)
-    return r_opt
+#most likelyhood estimation
+def MLE(xdata): 
+    normal = lambda x, mu, sig : np.exp(-(x-mu)**2/(2*sig**2))/(sig*np.sqrt(2*np.pi))
+    return  (xdata.mean(), xdata.std())
 
-def random_date():
-    latest  = datetime.date.today()
-    earliest = datetime.date(year=latest.year-20, month=latest.month, day=latest.day-1)
-    delta = latest - earliest
-    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds    
-    random_second = random.randrange(int_delta)
-    return earliest + datetime.timedelta(seconds = random_second)
+#calculating model parameters
+def calculate_model(xdata, ydata):
+    a, err = regresion(xdata, ydata)
+    err = (ydata - err) 
+    mu, sigma = MLE(err)
+    return(a, mu, sigma)
 
-def sim (a):
-    if a not in dates: 
-        for i in range(2):
-            dd = (int(a[8:10]))
-            dd += 1
-            a = a[:9] + str(dd)
-            if a in dates:
-                break
-        if a not in dates:
-            print("data poza zakresem lub w zlym formacie")
-            return 
+#prediction will be the value  of a linear model a[1]*x + a[0], plus randolmly generated error from normal distribiution with given mu and sigma
+def prediction(x, model): 
+    a, mu, sigma = model
+    return a[1]*x+a[0]+np.random.normal(loc = mu, scale = sigma)
 
-    N = dates.index(a)
-    
-    a,b = regresion(prices[N:len(dates)-5], prices[N+5:])   
-    predicted_prices = []
-    
-    for x in prices[N-p:N]: 
-        predicted_prices.append(a*x+b) 
+#simulation for n days
+def simulation(x0, model, n):
+    predicted_prices = list(prediction(x0, model))
+    for i in range(n-1):
+        predicted_prices.append(prediction(predicted_prices[-1], model))
     return predicted_prices
 
+def error(a,b):
+    err = 0
+    for i in range(len(a)):
+        err+=(b[i]-a[i])**2
+    return (err)
+
 API_KEY = 'ESJGNOSQE67NP8OM'
-API2 = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=" + API_KEY
+API = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=" + API_KEY
 
-data = API_call(API2)['Time Series (Daily)']
+#extracting data 
+data = API_call(API)['Time Series (Daily)']
 dates = list(data.keys())
-
 prices=[]
 for i in range(len(dates)):
     prices.append(float(data[dates[i]]['4. close']))
 
-keys = (data[dates[0]].keys())
+#geting data for learning
+d = '2019-01-02'
+d = dates.index(d)
+N = len(prices)
+days = N-d
+xdata = np.array(prices[0:d])
+ydata = np.array(prices[1:d+1])
 
+#learning model
+model = calculate_model(xdata, ydata)
+a, mu, sigma = model
 
-a = '2019-05-30'
+#real data
+real_data = prices[d:]
+plt.plot(real_data)
 
-N = dates.index(a)
-p = len(prices) - N
-predicted_prices = sim(a)
-
-avg_pred = predicted_prices.copy()
-for i in range(99):
-    pred = None
-    while pred == None:
-        pred = sim(str(random_date()))
-    for j in range(len(pred)):
-        avg_pred[j] += pred[j]
-
-for i in range(len(avg_pred)):
-    avg_pred[i] = avg_pred[i]/100
-
-plt.plot(prices[N:])
+#predicting 
+predicted_prices = simulation(prices[-1], model, days)
 plt.plot(predicted_prices)
-plt.plot(avg_pred)
-plt.legend(['real' , 'predicted', 'average prediction'])
+plt.legend(['real data', 'prediction'])
 plt.show()
 
-dif = (np.array(prices[N:]) - np.array(predicted_prices))
-u = 2
-dif = np.power(dif, u)
-print(sum(dif))
+print(error(predicted_prices, real_data))
 
-dif = (np.array(prices[N:]) - np.array(avg_pred))
-u = 2
-dif = np.power(dif, u)
-print(sum(dif))
+#100 simulations
+predicted_sum = np.zeros_like(predicted_prices)
+for i in range(100):
+    predicted_prices = simulation(prices[-1], model, days)
+    predicted_sum += predicted_prices
+predicted_avg = predicted_sum/100
+plt.title("100 simulations")
+plt.plot(predicted_avg)
+
+plt.show()
+print(error(predicted_avg, real_data))
